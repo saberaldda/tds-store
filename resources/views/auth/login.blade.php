26842c1e1e3c4cc10039db309a920e55bdf8a200
Saber <saberaldda@gmail.com>
2023-10-05 20:46:09 +0300

    demo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
             <div>
                 <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', 'admin@tds.com')" required autofocus />
             </div>
 
             <!-- Password -->
@@ -29,6 +29,7 @@
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
+                                :value="old('password', 'password')"
                                 required autocomplete="current-password" />
             </div>
 
